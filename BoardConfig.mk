dc3966e983ac5ec69d72dd30c94d7e4623b8692c
ssundeeheydarpour-oss <ssundeeheydarpour@gmail.com>
2026-07-23 14:46:45 +0330

    Port a5xelte tree for OrangeFox R14.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,8 @@
-
 include device/samsung/universal7580-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := lineageos_a5xelte_defconfig
 
-#BOARD_MKBOOTIMG_ARGS += --dt device/samsung/a5xelte/prebuilt/dt.img --board SRPOJ08A000KU
 BOARD_MKBOOTIMG_ARGS += --board SRPOJ08A000KU
 
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
-
-BOARD_SEPOLICY_DIRS += device/samsung/universal7580-common/sepolicy
--- omni_a5xelte.mk
+++ omni_a5xelte.mk
@@ -1,19 +1,15 @@
-# Release name
 PRODUCT_RELEASE_NAME := a5xelte
-# Inherit from the common Open Source product configuration
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit Exynos7580 common device configuration
 $(call inherit-product, device/samsung/universal7580-common/device-common.mk)
 
-# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
+    charger_res_images \
+    charger
 
-## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a5xelte
 PRODUCT_NAME := omni_a5xelte
 PRODUCT_BRAND := samsung
